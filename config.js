f0d015dcb8cbffb951696339ef350e03046b66a7
ikyyybitzzxx <106673608+ikyyybitzzxx@users.noreply.github.com>
2022-07-24 19:37:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -27,8 +27,8 @@ global.webm = 'https://saweria.co/arullOfc'
 global.price1 = '𝐍𝐈𝐇 𝐁𝐀𝐍𝐆 𝐋𝐈𝐒𝐓 𝐒𝐄𝐖𝐀𝐍𝐘𝐀\n\n》1 MINGGU Rp 5.000\nMAKSIMAL 1 GRUP\n》1 BULAN  Rp 10.000\nMAKSIMAL 1 GRUP \n》 Rp 20.000\nMAKSIMAL 1 GRUP + PREMIUM\n》PERMANEN Rp 30.000\nMAKSIMAL 3 GRUP\n》4 BULAN  Rp 25.000\nMAKSIMAL 2 GRUP + PREMIUM\n\nMAU NAMBAH GRUP LAGI \nPERMANEN Rp 25.000\nMAKSIMAL 2 GRUP'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/f175cf27445e4a8d81c94.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/f175cf27445e4a8d81c94.jpg' //ini juga ganti 
+global.fotonya1 = 'https://telegra.ph/file/ccc331b1f943e068b3b11.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/ccc331b1f943e068b3b11.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = '↻CHIKA-MD'
@@ -37,7 +37,7 @@ global.wm2 = 'ᯤ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁 𝙱𝚈 Kyy ×፝֟͜×'
 global.wm3 = 'ᯤ MFX'
 global.htki = '––––––『' 
 global.htka = '』––––––'
-global.media = 'https://telegra.ph/file/f175cf27445e4a8d81c94.jpg'
+global.media = 'https://telegra.ph/file/ccc331b1f943e068b3b11.jpg'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wait = '⏳Tunggu Sedang Proses...'
 global.eror = '_*🚨 Server Bot Sedang Error*_'
